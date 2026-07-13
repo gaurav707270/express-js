8f2cc8da5179a6c4b3142ef933a7f5d7029bda67
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-07-13 12:47:08 +0530

    13/07/2026

--- express_js_day08/index.js
+++ express_js_day08/index.js
@@ -1,20 +1,26 @@
 import express from "express";
+import path from "path";
 const app = express();
+
+const absPath = path.resolve("form.html")
 
 app.get("",(req,res) =>{
     res.send("this is home page")
 })
 
 app.get("/login",(req,res) =>{
-    res.send("this is login page")
+    res.sendFile(absPath)
 })
+
+app.get("/submite",(req,res) =>{
+    res.send("this is submite page")
+})
+
 
 app.get("/user",(req,res) =>{
     res.send("this is user page")
 })
 
-app.get("/products",(req,res) =>{
-    res.send("this is products page")
-})
+
 
 app.listen(3000)

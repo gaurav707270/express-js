702425929bc8e6229267c1dab68f06713e48a06e
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-07-19 11:26:13 +0530

    19/07/2026

---
+++ express_js_day10/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+
+const app = express();
+
+const url = "mongodb://localhost:27017";
+const dbName = "testdb";
+
+const client = new MongoClient(url);
+
+app.set("view engine", "ejs");
+
+// Connect Database Once
+async function dbConnection() {
+    try {
+        await client.connect();
+        console.log("MongoDB Connected");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+dbConnection();
+
+app.get("/api", async (req, res) => {
+
+    const db = client.db(dbName);
+    const collection = db.collection("post");
++
+    const result = await collection.find().toArray();
+
+    console.log(typeof (result));
+
+    res.render("post", {
+        posts: result
+    });
+
+
+});
+
+app.listen(3200, () => {
+    console.log("Server running at http://localhost:3200");
+});

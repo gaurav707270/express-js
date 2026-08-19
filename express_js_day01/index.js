7a2cf648abf5e6cca4c81b39d19e4668f2f4272e
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-19 09:58:21 +0530

    Update index.js

--- express_js_day01/index.js
+++ express_js_day01/index.js
@@ -1,6 +1,6 @@
 // const express = require("express");
 
-const app = express()
+// const app = express()
 
 app.get("/", (req, res) => {
     res.send("<h1> this is home page base node js and express js example<h1/>")

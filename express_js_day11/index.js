22ef18d01c49d655639d2dcf14c735887f9beb7e
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-28 12:06:06 +0530

    Comment out database connection in index.js
    Comment out the database connection line for testing.

--- express_js_day11/index.js
+++ express_js_day11/index.js
@@ -13,7 +13,7 @@
 
 // const dbConnection = async () => {
     // try {
-        await client.connect();
+        // await client.connect();
         console.log("Database connected");
     } catch (err) {
         console.log(err);

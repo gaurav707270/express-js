ab211d02fd06c6e72d2d38cfcf23e9e4fb95b2ce
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-03 17:34:16 +0530

    Comment out home page response

--- express_js_day02/index.js
+++ express_js_day02/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 // const app = express()
 
 // app.get("/", (req, res) => {
-    res.send("this is home pAGE")
+    // res.send("this is home pAGE")
 })
 
 app.get("/about", (req, res) => {

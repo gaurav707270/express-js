7d262c148b378b472bc73560ed441fc8082f1a27
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-03 17:35:01 +0530

    Update index.js

--- express_js_day02/index.js
+++ express_js_day02/index.js
@@ -5,7 +5,7 @@ import express from "express"
 const app = express()
 
 // app.get("/", (req, res) => {
-    // res.send("this is home pAGE")
+    res.send("this is home pAGE")
 })
 
 app.get("/about", (req, res) => {

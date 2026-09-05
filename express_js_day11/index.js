976ada1d0ef0c5342157f414e09d752fda65e7f2
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-05 11:23:56 +0530

    Update index.js

--- express_js_day11/index.js
+++ express_js_day11/index.js
@@ -4,7 +4,7 @@ import { MongoClient } from "mongodb";
 // const app = express();
 
 // app.set("view engine", "ejs");
-// app.set("views", "./views");
+app.set("views", "./views");
 
 const url = "mongodb://localhost:27017";
 const dbName = "userList";

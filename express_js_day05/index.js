96f962706d6828b0f00a59a5ac02151888bf65dd
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-23 19:10:59 +0530

    Update index.js

--- express_js_day05/index.js
+++ express_js_day05/index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
     // res.send(`
-        // <form action="/signup" method="post">
+        <form action="/signup" method="post">
         <h1> Lodin Form</h1>
         <input type="email" placeholder="enter the email" />
         <input type="password" placeholder="enter the password" />
